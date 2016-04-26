a89890651d8cdebd35dd79495ad1263126be0796
Leandro Ostera <leandro@ostera.io>
2016-04-26 22:30:53 +0200

    Put record in a shareable place

--- include/nobel.hrl
+++ include/nobel.hrl
@@ -1,10 +1,10 @@
 -record(observation, {
           name,
-          started_at=timestamp(),
+          started_at=not_started,
           finished_at=not_finished,
           time_delta=not_finished,
           result=no_result,
-          function=no_function,
+          predicate=no_function,
           pid=no_pid,
           type=candidate
          }).
