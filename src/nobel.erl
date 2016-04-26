6caef21d670c2c32022dd7dc8e949aa2506c6bd4
Leandro Ostera <leandro@ostera.io>
2016-04-26 22:31:23 +0200

    Prawgress !

--- src/nobel.erl
+++ src/nobel.erl
@@ -5,6 +5,7 @@
 -include("../include/nobel.hrl").
 
 sample(I) ->
+  {ok, Server} = nobel_server:start_link(),
   Wait = fun (Id, Time, Return) ->
              { io_lib:format("Experiment #~p", [Id]),
                fun () ->
@@ -14,14 +15,13 @@ sample(I) ->
                end
              }
          end,
-  experiment("Test Experiment",
-             [Wait(0, 2, true),
-             [ Wait(N, N, N) || N <- lists:seq(1,I-1) ]
-             ]).
+  experiment(Server,
+             "Test Experiment",
+             Wait(0, 2, true),
+             [ Wait(N, N, N) || N <- lists:seq(1,I-1) ]).
 
-experiment(Name, [Control|Candidates]) ->
-  gen_server:cast(nobel_server, {measure, {Name, Control, Candidates}}),
+experiment(Server, Name, Control, Candidates) ->
+  gen_server:cast(Server, {measure, {Name, Control, Candidates}, self()}),
   receive
-    {control, Observation} -> Observation#observation.result
-  after infinity -> {error, timeout}
+    {measurement, #observation{type=control}=Observation} -> Observation#observation.result
   end.
--- src/nobel_server.erl
+++ src/nobel_server.erl
@@ -1,4 +1,4 @@
--module(nobel).
+-module(nobel_server).
 
 -compile([export_all]).
 
@@ -6,62 +6,96 @@
 
 -include("../include/nobel.hrl").
 
-experiment(Name, Foos) ->
-  Experiments = [ run(hd(Foos), true) |
-                  lists:map(fun (F) -> run(F) end, tl(Foos)) ],
-  io:format("~p (~p experiments)", [Name, length(Foos)]),
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
+-record(state, {}).
+
+start_link() ->
+  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+init([]) ->
+  {ok, #state{}}.
+
+handle_call(_Request, _From, State) ->
+  {reply, ignored, State}.
+
+handle_cast({measure, {Name, Control, Candidates}, Scientist}, State) ->
+  experiment(Name, Control, Candidates, Scientist),
+  {noreply, State}.
+
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+terminate(_Reason, _State) ->
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+experiment(Name, Control, Candidates, Scientist) ->
+  Observations = [ run(Control, true) |
+                  lists:map(fun (F) -> run(F) end, Candidates) ],
+  io:format("~p (~p experiments)", [Name, length(Candidates)+1]),
   io:format("\n\n"),
   io:format("---------------------------------------------------------------------------------\n"),
-  io:format("Name\t\tStart\t\tEnd\t\tTime\tResult Experiment\n"),
+  io:format("Name\t\tStart\t\tEnd\t\tTime\tResult Observation\n"),
   io:format("---------------------------------------------------------------------------------\n"),
-  Measurements = collect(Experiments),
-  {Name, Measurements}.
+  collect(Observations, Scientist).
 
-collect(Experiments) -> collect(Experiments, []).
-collect([], Results) -> Results;
-collect(Experiments, Results) ->
+collect(Observations, Scientist) -> collect(Observations, Scientist, []).
+collect([], Scientist, Results) -> Scientist ! {results, Results};
+collect(Observations, Scientist, Results) ->
   receive
     {Pid, Result} ->
-      RemainingExperiments = lists:filter(fun (Experiment) ->
-                                           Pid =/= Experiment#measurement.pid
-                                       end, Experiments),
-      Experiment = hd(lists:filter( fun (Experiment) ->
-                                  Pid =:= Experiment#measurement.pid
-                              end, Experiments)),
+      RemainingObservations = lists:filter(fun (Observation) ->
+                                           Pid =/= Observation#observation.pid
+                                       end, Observations),
+      Observation = hd(lists:filter( fun (Observation) ->
+                                  Pid =:= Observation#observation.pid
+                              end, Observations)),
       Done = timestamp(),
-      NewExperiment = Experiment#measurement{
+      NewObservation = Observation#observation{
                         result=Result,
                         finished_at=Done,
-                        delta=Done-Experiment#measurement.started_at
+                        time_delta=Done-Observation#observation.started_at
                        },
-      print_measurement(NewExperiment),
-      AccResults = [ NewExperiment | Results ],
-      collect(RemainingExperiments, AccResults)
+      Scientist ! {measurement, NewObservation},
+      print_measurement(NewObservation),
+      AccResults = [ NewObservation | Results ],
+      collect(RemainingObservations, Scientist, AccResults)
   end.
 
 timestamp() -> erlang:monotonic_time(seconds).
 
-run(Spec) -> run(Spec, false).
+run(Spec) -> run(Spec, candidate).
 
-run({Name, F}, Control) when is_function(F) ->
+run({Name, F}, Type) when is_function(F) ->
   Collector=self(),
   Pid=erlang:spawn( fun () ->
                         Result = F(),
                         Collector ! { self(), Result }
                     end ),
-  #measurement{
+  #observation{
      name=Name,
-     foo=F,
+     predicate=F,
      pid=Pid,
-     control=Control
+     type=Type
   }.
 
 print_measurement(M) ->
   io:format("~s\t~p\t~p\t~p\t~p\t~p\n", [
-    M#measurement.name,
-    M#measurement.started_at,
-    M#measurement.finished_at,
-    M#measurement.delta,
-    M#measurement.result,
-    not M#measurement.control
+    M#observation.name,
+    M#observation.started_at,
+    M#observation.finished_at,
+    M#observation.time_delta,
+    M#observation.result,
+    M#observation.type
   ]).
